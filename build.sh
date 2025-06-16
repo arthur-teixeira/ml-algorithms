e3273736c281fa811b33efb5671be6b36cf3795d
arthur-teixeira <arthurcarvalhot@yahoo.com.br>
2025-06-16 13:57:30 -0300

    Link with raylib

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-gcc main.c -Ofast -lm -o net 
+
+set -xe
+
+CFLAGS="-Wall -Wextra -ggdb -Ofast `pkg-config --cflags raylib`"
+LIBS="`pkg-config --libs raylib` -lm"
+
+clang $CFLAGS -o ./net ./*.c $LIBS -L./bin/
